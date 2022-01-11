be0ea11a1ae912d3d21e4e7f06d3f6271fb31922
Brick McDermott <mikdermit@icloud.com>
2022-01-11 14:59:54 -0500

    Updates

---
+++ precache-manifest.62c801f8b28ebc56671d8a424e400b97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a081cf2b519cd7192d08f57f7ad0bbae",
+    "url": "/user-posts/index.html"
+  },
+  {
+    "revision": "88695f906bae86be6c1f",
+    "url": "/user-posts/static/css/main.b04ba9dc.chunk.css"
+  },
+  {
+    "revision": "52006c904c913a9819b2",
+    "url": "/user-posts/static/js/2.b970e8f4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/user-posts/static/js/2.b970e8f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88695f906bae86be6c1f",
+    "url": "/user-posts/static/js/main.be787064.chunk.js"
+  },
+  {
+    "revision": "7a76c3654e411b0cad99",
+    "url": "/user-posts/static/js/runtime-main.85c5d7e2.js"
+  }
+]);
